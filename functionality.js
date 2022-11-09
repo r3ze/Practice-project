0741db07dc98121f3bd5e09ce5ab9b9e3a86565d
Reze <112688309+r3ze@users.noreply.github.com>
2022-11-09 17:19:51 +0800

    unfinished

--- functionality.js
+++ functionality.js
@@ -1,23 +1,65 @@
 function calculateBMI(){
-    
-    var height = parseInt(document.getElementById("height").value);
-    var weight = parseInt(document.getElementById("weight").value);
-   var meters = height/100
-   var metersS = meters*meters
-var bmi = weight/metersS
-var status
-if(bmi<18.5){
-    status = "underweight"
+    var heightmetric = document.getElementById("height").value;
+    var weightmetric = document.getElementById("weight").value
+    var height = document.getElementById("txt-height").value;
+    var weight = document.getElementById("txt-weight").value;
+     //Height 
+     if(weightmetric=="kilograms"){
+        
+   }
+     var bmi_result;    
+     if(heightmetric=="centimeters"){ 
+      if(weightmetric=="kilograms"){
+      height/=100;
+      bmi_result = weight/(height*height);
+      }
+      else if(weightmetric=="pounds"){
+         height/=100;
+         weight/= 2.205;
+         bmi_result = weight/(height*height);
+      }
+      }
+     else if(heightmetric=="meters"){
+      if(weightmetric=="kilograms"){
+         bmi_result = weight/(height*height)
+         }
+         else if(weightmetric=="pounds"){
+            weight/= 2.205;
+            bmi_result = weight/(height*height);
+         }
+      
+      }
+      else if(heightmetric=="inches"){    
+         if(weightmetric=="kilograms"){
+            height/=39.37;
+            bmi_result = weight/(height*height);
+         }
+         else if(weightmetric=="pounds"){
+            height/=39.37;
+            weight/= 2.205;
+            bmi_result = weight/(height*height);
+         }
+       
+      }
+      else if(heightmetric=="feet"){
+         if(weightmetric=="kilograms"){
+            height/=3.281;
+            bmi_result = weight/(height*height);
+         }
+         else if(weightmetric=="pounds"){
+            height/=3.281;
+            weight/= 2.205;
+            bmi_result = weight/(height*height);
+         }
+      }
+
+      var result = bmi_result.toFixed(4);
+   document.getElementById("BMI").innerHTML = "Your BMI: "+result;
 }
-else if(bmi<=24.9){
-    status = "healthy"
-}
-else if(bmi<=29.9){
-    status = "overweight"
-}
-else{
-    status = "obese";
-}
-    document.getElementById("result").value = parseFloat(bmi).toFixed(2)+".";
-    document.getElementById("status").value = status+"."
+function reset(){
+   document.getElementById("height").selectedIndex = "0";
+   document.getElementById("weight").selectedIndex = "0";
+   document.getElementById("BMI").innerHTML = "Your BMI:";
+   document.getElementById("txt-height").value = "";
+   document.getElementById("txt-weight").value = "";
 }
